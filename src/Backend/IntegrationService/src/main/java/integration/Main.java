7461bc491ec4d301b99c09b3f62ce977ffcf07fa
Raghav Matta <mattaraghav118@gmail.com>
2018-09-30 10:09:38 +0000

    Update Main.java

--- src/Backend/IntegrationService/src/main/java/integration/Main.java
+++ src/Backend/IntegrationService/src/main/java/integration/Main.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 
 public class Main {
 	public static void main(String[] args) {
-		//Adding the scheduled task classes to the spring application to run in background threads.
+		//Adding the scheduled task classes to the spring application to run in background threads
 		SpringApplication.run(new Object[] { CreateOrderProcessTask.class, UpdateProductProcessTask.class }, new String[0]);
 	}
 }
